402737b384ccff5d8e73480739dc91c39dbdf68d
ESPP-ZDT <toplelfaget@gmail.com>
2022-12-24 01:59:54 +0100

    added profile picture display in the header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -6,22 +6,22 @@ import { Form, FormControl } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../actions/userActions";
+import { Image } from "react-bootstrap";
 
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
-function BasicExample({setSearch}) {
+function BasicExample({ setSearch }) {
   let navigate = useNavigate();
 
   const dispatch = useDispatch();
 
-  const userLogin =useSelector(state => state.userLogin)
-  const {userInfo} = userLogin
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-  const logoutHandler = () =>{
-    dispatch(logout())
-    navigate("/")
-
-  }
+  const logoutHandler = () => {
+    dispatch(logout());
+    navigate("/");
+  };
 
   return (
     <Navbar bg="light" expand="lg">
@@ -38,35 +38,53 @@ function BasicExample({setSearch}) {
                 type="text"
                 placeholder="Search"
                 className="mr-sm-2"
-                onChange={(e) =>setSearch(e.target.value)}
+                onChange={(e) => setSearch(e.target.value)}
               />
             </Form>
           </Nav>
-          {userInfo? <Nav>
-          <Link to="/profile">
-            <Button variant="primary" size="xs">my profile</Button>
-            
-            </Link>
-            <Link to="/mynotes">
-            <Button variant="primary" size="xs">my Notes</Button>{' '}
-            </Link>
-            
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item onClick={logoutHandler}>
-              Logout
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>: <Nav>
-          <Link to="/login">
-            <Button variant="primary" size="xs">Login</Button>{' '}
-            </Link>
-            </Nav>}
+          {userInfo ? (
+            <Nav>
+              <Image
+                src={userInfo.pic}
+                roundedCircle
+                width="40"
+                height="40"
+                className="mr-2"
+              />
+              <Link to="/profile">
+                <Button variant="primary" size="xs">
+                  my profile
+                </Button>
+              </Link>
+              <Link to="/mynotes">
+                <Button variant="primary" size="xs">
+                  my Notes
+                </Button>{" "}
+              </Link>
+
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          ) : (
+            <Nav>
+              <Link to="/login">
+                <Button variant="primary" size="xs">
+                  Login
+                </Button>{" "}
+              </Link>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
